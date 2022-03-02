c0f7f59f80be9d326d9455742ab8ea480727593c
Akshay Sridhar <asridhar@caltech.edu>
2022-03-02 13:09:56 -0800

    modified:   test/test_cases/run_3d_rising_bubble.jl

--- test/test_cases/run_3d_rising_bubble.jl
+++ test/test_cases/run_3d_rising_bubble.jl
@@ -145,7 +145,7 @@ function run_3d_rising_bubble(
             Δρq_tot = (∫ρq_tot_e - ∫ρq_tot_0) ./ ∫ρq_tot_0 * 100
             if FT == Float32
                 @test abs(Δρ) < 3e-5
-                @test abs(Δρe_tot) < 1e-5
+                @test abs(Δρe_tot) < 1.5e-5
                 @test abs(Δρq_tot) < 1e-3
             else
                 @test abs(Δρ) < 1e-12
