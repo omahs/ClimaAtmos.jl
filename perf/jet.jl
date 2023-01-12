c63db6a1c56abd6bda8e1e7f3b9da4f91d95c950
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-01-11 16:56:48 -0800

    Update and add JET test to buildkite

--- perf/jet.jl
+++ perf/jet.jl
@@ -1,15 +1,24 @@
-import Profile
+ca_dir = joinpath(dirname(@__DIR__));
+include(joinpath(ca_dir, "examples", "hybrid", "cli_options.jl"));
 
 ENV["CI_PERF_SKIP_RUN"] = true # we only need haskey(ENV, "CI_PERF_SKIP_RUN") == true
 
-filename = joinpath(dirname(@__DIR__), "examples", "hybrid", "driver.jl")
+import RRTMGP
+import ClimaComms
+import CUDA
 
-# Uncomment for customizing specific jobs / specs:
-# dict = parsed_args_per_job_id(; trigger = "benchmark.jl"); # if job_id uses benchmark.jl
-# dict = parsed_args_per_job_id();                           # if job_id uses driver.jl
-# parsed_args = dict["sphere_aquaplanet_rhoe_equilmoist_allsky"];
+RRTMGP.Device.array_type() = CUDA.has_cuda_gpu() ? CUDA.CuArray : Array
+ClimaCore.Device.device() = ClimaComms.CPU()
 
-try
+filename = joinpath(ca_dir, "examples", "hybrid", "driver.jl")
+dict = parsed_args_per_job_id(; trigger = "benchmark.jl")
+parsed_args_prescribed = parsed_args_from_ARGS(ARGS)
+
+# Start with performance target, but override anything provided in ARGS
+parsed_args_target = dict["perf_target_unthreaded"];
+parsed_args = merge(parsed_args_target, parsed_args_prescribed);
+
+try # capture integrator
     include(filename)
 catch err
     if err.error !== :exit_profile
@@ -20,4 +29,5 @@ end
 import JET
 
 OrdinaryDiffEq.step!(integrator) # Make sure no errors
-JET.@test_opt OrdinaryDiffEq.step!(integrator)
+# JET.@test_opt OrdinaryDiffEq.step!(integrator)
+JET.@report_opt OrdinaryDiffEq.step!(integrator)
