2ac38c86ade10620a4eb2216785ba5a2110325e0
Akshay Sridhar <asridhar@caltech.edu>
2023-05-15 16:34:59 -0700

    modified:   src/utils/cli_options.jl modified:   src/utils/type_getters.jl

--- src/utils/type_getters.jl
+++ src/utils/type_getters.jl
@@ -116,7 +116,7 @@ function get_spaces(parsed_args, params, comms_ctx)
             lon = data["longitude"][:]
             lat = data["latitude"][:]
             # Apply Smoothing
-            smooth_degree = 5
+            smooth_degree = Int(parsed_args["topo-smoothing"])
             esmth = imfilter(zlevels, Kernel.gaussian(smooth_degree))
             linear_interpolation(
                 (lon, lat),
