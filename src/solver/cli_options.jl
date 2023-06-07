a3ee9942ba534cb1ef40ea452bbde16fdb4f00f2
Akshay Sridhar <asridhar@caltech.edu>
2023-06-07 09:20:03 -0700

    modified:   .buildkite/pipeline.yml 	modified:   src/solver/cli_options.jl 	modified:   src/solver/type_getters.jl 	modified:   src/utils/common_spaces.jl

--- src/solver/cli_options.jl
+++ src/solver/cli_options.jl
@@ -308,7 +308,11 @@ function argparse_settings()
         help = "Define the surface elevation profile [`NoWarp`,`Earth`,`DCMIP200`,`Agnesi`]"
         arg_type = String
         default = "NoWarp"
-        "--topo-smoothing"
+        "--topo_smoothing"
+        help = "Choose whether to order-2 smoothing on the LGL mesh"
+        arg_type = Bool
+        default = false
+        "--smoothing_order"
         help = "Define the surface smoothing kernel factor (integer) [`3 (default)`]"
         arg_type = Int
         default = 3
